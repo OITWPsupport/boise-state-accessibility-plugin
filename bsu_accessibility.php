364a04fe0b9adb9671b41de60eefc330c7e61ae4
David M. Lentz <davidmlentz@gmail.com>
2016-10-07 16:21:36 -0600

    Further extends the plugin to address WCAG errors being caused by Event Espresso. THESE CHANGES NEED TESTING.

--- bsu_accessibility.php
+++ bsu_accessibility.php
@@ -112,6 +112,32 @@ function bsu_accessibility($content){
 		}
 	}
 
+	$tables = $dom->getElementsByTagName('table');
+	foreach($tables as $table) {
+		if ($table->getAttribute('class') == 'fc-border-separate') {
+			// Remove the style attribute from the table tag to get rid of any
+			// presentational attributes.
+			$field->setAttribute('style', '');
+			// TODO?: need to add "width:100%" (which we just removed from the table tag)
+			// on to the class 'fc-border-separate'
+		}
+	}
+
+	$tablerows = $dom->getElementsByTagName('tr');
+	foreach($tablerows as $tablerow) {
+		if ($tablerow->getAttribute('class') == 'fc-day-header') {	
+
+			// TODO: actually, this'll be only one of several class designations on this 
+			// tag, so we shouldn't do a literal string compare.
+			
+			$tablerow->setAttribute('style', '');
+
+			// TODO: need to add "width: 92px;" (which we just removed from the TR tag)
+			// on to the class 'fc-day-header'
+
+		}
+	}
+
 	// SAVING THIS FOR A FUTURE VERSION. Does not work reliably right now:
 	// A pair of A tags with only images inside them will disappear, images and all.
 	// C14N may be a way to handle this, but it returns the string including the tags, 
